ce7a4486e185d34a309eb08d744a2ca3d3519c6c
Charlie Holtz <charlieholtz@gmail.com>
2024-08-02 17:15:02 -0700

    mess with start script

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# create virtual env if not exists
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+fi
+
 # Activate the virtual environment
 source venv/bin/activate
 
