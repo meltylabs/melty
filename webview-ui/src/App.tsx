230a68054f3af84ff4b97951e2cfffa85c718d6b
Charlie Holtz <charlieholtz@gmail.com>
2024-08-07 13:25:19 -0700

    fix dupe button

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -16,7 +16,6 @@ import {
   Navigate,
   useNavigate,
 } from "react-router-dom";
-import { Button } from "./components/ui/button";
 
 import * as Diff2Html from "diff2html";
 import "diff2html/bundles/css/diff2html.min.css";
