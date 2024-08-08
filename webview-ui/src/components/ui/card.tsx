2574c599afc8c99a25dc6e6ac387f8e39bebcef7
Charlie Holtz <charlieholtz@gmail.com>
2024-08-07 21:05:31 -0700

    turn off rounded cards

--- webview-ui/src/components/ui/card.tsx
+++ webview-ui/src/components/ui/card.tsx
@@ -8,10 +8,7 @@ const Card = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
-      className
-    )}
+    className={cn(" border bg-card text-card-foreground shadow-sm", className)}
     {...props}
   />
 ));
