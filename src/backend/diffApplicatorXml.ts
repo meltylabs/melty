9142d913496a300f21a5807af5a4989a827290e0
Jackson de Campos <jacksondecampos@gmail.com>
2024-08-10 13:36:42 -0700

    stop throwing when diffs don't apply, and return raw output

--- src/backend/diffApplicatorXml.ts
+++ src/backend/diffApplicatorXml.ts
@@ -1,6 +1,7 @@
-import { PseudoCommit, GitRepo, SearchReplace } from '../types';
-import * as pseudoCommits from './pseudoCommits';
-import * as searchReplaces from './searchReplace';
+import { PseudoCommit, GitRepo, SearchReplace } from "../types";
+import * as pseudoCommits from "./pseudoCommits";
+import * as searchReplaces from "./searchReplace";
+import * as vscode from "vscode";
 
 const CODE_FENCE = ["<CodeChange>", "</CodeChange>"];
 const DIFF_OPEN = "<<<<<<< SEARCH";
@@ -11,37 +12,40 @@ const DIFF_PIECES = [DIFF_OPEN, DIFF_DIVIDER, DIFF_CLOSE];
 type Section = "search" | "replace" | "codeChange" | "topLevel";
 
 export const testExports = {
-    applySearchReplace
+  applySearchReplace,
 };
 
-export function applySearchReplaceBlocks(gitRepo: GitRepo, pseudoCommit: PseudoCommit, searchReplaceBlocks: SearchReplace[]): PseudoCommit {
-    return searchReplaceBlocks.reduce((pseudoCommit, searchReplace) => {
-        return applySearchReplace(gitRepo, pseudoCommit, searchReplace);
-    }, pseudoCommit);
+export function applySearchReplaceBlocks(
+  gitRepo: GitRepo,
+  pseudoCommit: PseudoCommit,
+  searchReplaceBlocks: SearchReplace[]
+): PseudoCommit {
+  return searchReplaceBlocks.reduce((pseudoCommit, searchReplace) => {
+    return applySearchReplace(gitRepo, pseudoCommit, searchReplace);
+  }, pseudoCommit);
 }
 
 function stripFilename(filename: string): string | undefined {
-    filename = filename.trim();
+  filename = filename.trim();
 
-    if (filename === "...") {
-        throw new Error(`Unable to get filename from: ${filename}`); // TODO: relax this to undefined
-    }
+  if (filename === "...") {
+    throw new Error(`Unable to get filename from: ${filename}`); // TODO: relax this to undefined
+  }
 
-    const startFence = CODE_FENCE[0];
-    if (filename.startsWith(startFence)) {
-        throw new Error(`Unable to get filename from: ${filename}`); // TODO: relax this to undefined
-    }
+  const startFence = CODE_FENCE[0];
+  if (filename.startsWith(startFence)) {
+    throw new Error(`Unable to get filename from: ${filename}`); // TODO: relax this to undefined
+  }
 
-    filename = filename.replace(/:$/, "");
-    filename = filename.replace(/^#/, "");
-    filename = filename.trim();
-    filename = filename.replace(/^`|`$/g, "");
-    filename = filename.replace(/^\*|\*$/g, "");
-    filename = filename.replace(/\\_/g, "_");
+  filename = filename.replace(/:$/, "");
+  filename = filename.replace(/^#/, "");
+  filename = filename.trim();
+  filename = filename.replace(/^`|`$/g, "");
+  filename = filename.replace(/^\*|\*$/g, "");
+  filename = filename.replace(/\\_/g, "_");
 
-    return filename;
+  return filename;
 }
-
 
 // function findFilename(lines: string[]): string | undefined {
 //     for (const line of lines.reverse()) {
@@ -53,139 +57,167 @@ function stripFilename(filename: string): string | undefined {
 //     return undefined;
 // }
 
-
 // function ensurePiece(piece: string, expected: string) {
 //     if (piece !== expected) {
 //         throw new Error(`Unexpected piece: ${piece} does not match ${expected}`);
 //     }
 // }
 
-function categorizeLine(rawLine: string): "srOpen" | "srDivider" | "srClose" | "ccOpen" | "ccClose" | "other" {
-    const line = rawLine.trim();
+function categorizeLine(
+  rawLine: string
+): "srOpen" | "srDivider" | "srClose" | "ccOpen" | "ccClose" | "other" {
+  const line = rawLine.trim();
 
-    const commandPieces = [DIFF_OPEN, DIFF_DIVIDER, DIFF_CLOSE, "<CodeChange", "</CodeChange>"];
-    const commandMatches = commandPieces.filter((piece) => line.includes(piece));
-    if (!commandMatches.length) {
-        return "other";
+  const commandPieces = [
+    DIFF_OPEN,
+    DIFF_DIVIDER,
+    DIFF_CLOSE,
+    "<CodeChange",
+    "</CodeChange>",
+  ];
+  const commandMatches = commandPieces.filter((piece) => line.includes(piece));
+  if (!commandMatches.length) {
+    return "other";
+  } else if (commandMatches.length > 1) {
+    throw new Error(`Line has multiple command pieces on it: ${line}`);
+  } else if (!line.startsWith(commandMatches[0])) {
+    throw new Error(`Line does not start with command piece: ${line}`);
+  } else {
+    switch (commandMatches[0]) {
+      case DIFF_OPEN:
+        return "srOpen";
+      case DIFF_DIVIDER:
+        return "srDivider";
+      case DIFF_CLOSE:
+        return "srClose";
+      case "<CodeChange":
+        return "ccOpen";
+      case "</CodeChange>":
+        return "ccClose";
+      default:
+        throw new Error(`Unexpected command piece: ${commandMatches[0]}`);
     }
-    else if (commandMatches.length > 1) {
-        throw new Error(`Line has multiple command pieces on it: ${line}`);
-    } else if (!line.startsWith(commandMatches[0])) {
-        throw new Error(`Line does not start with command piece: ${line}`);
-    } else {
-        switch (commandMatches[0]) {
-            case DIFF_OPEN: return "srOpen";
-            case DIFF_DIVIDER: return "srDivider";
-            case DIFF_CLOSE: return "srClose";
-            case "<CodeChange": return "ccOpen";
-            case "</CodeChange>": return "ccClose";
-            default: throw new Error(`Unexpected command piece: ${commandMatches[0]}`);
-        }
-    }
+  }
 }
 
 function nextSection(currentSection: Section, nextSection: Section): Section {
-    const allowedTransitions: Record<Section, Section[]> = {
-        "search": ["replace"],
-        "replace": ["codeChange"],
-        "codeChange": ["topLevel", "search"],
-        "topLevel": ["codeChange"],
-    };
-    if (!allowedTransitions[currentSection].includes(nextSection)) {
-        throw new Error(`Unexpected next section: ${nextSection}`);
-    }
-    return nextSection;
+  const allowedTransitions: Record<Section, Section[]> = {
+    search: ["replace"],
+    replace: ["codeChange"],
+    codeChange: ["topLevel", "search"],
+    topLevel: ["codeChange"],
+  };
+  if (!allowedTransitions[currentSection].includes(nextSection)) {
+    throw new Error(`Unexpected next section: ${nextSection}`);
+  }
+  return nextSection;
 }
 
 /**
  * Splits the response into search/replace blocks and message chunks.
- * 
+ *
  * @param content The content to split
  * @param partialMode In partial mode, we expect a partial response, so we're more lenient in parsing
  * @returns The search/replace blocks and message chunks
  */
-export function splitResponse(content: string, partialMode: boolean): { searchReplaceList: SearchReplace[], messageChunksList: string[] } {
-    const searchReplaceList: SearchReplace[] = [];
-    const messageChunksList: string[] = [""];
-    let currentFile: string | null | undefined;
-    let currentSearch: string[] = [];
-    let currentReplace: string[] = [];
-    let currentSection: Section = "topLevel";
+export function splitResponse(
+  content: string,
+  partialMode: boolean
+): { searchReplaceList: SearchReplace[]; messageChunksList: string[] } {
+  const searchReplaceList: SearchReplace[] = [];
+  const messageChunksList: string[] = [""];
+  let currentFile: string | null | undefined;
+  let currentSearch: string[] = [];
+  let currentReplace: string[] = [];
+  let currentSection: Section = "topLevel";
 
-    const lines = content.split('\n');
+  const lines = content.split("\n");
 
-    for (const line of lines) {
-        switch (categorizeLine(line)) {
-            case "ccOpen":
-                messageChunksList.push("");
+  for (const line of lines) {
+    switch (categorizeLine(line)) {
+      case "ccOpen":
+        // // DISABLED -- parsed message strategy
+        // messageChunksList.push("");
 
-                try {
-                    currentFile = extractFileName(line);
-                } catch (e) {
-                    if (partialMode) {
-                        currentFile = null;
-                    } else {
-                        throw e;
-                    }
-                }
-                currentSection = nextSection(currentSection, "codeChange");
-                break;
-            case "srOpen":
-                currentSection = nextSection(currentSection, "search");
-                break;
-            case "srDivider":
-                currentSection = nextSection(currentSection, "replace");
-                break;
-            case "srClose":
-                if (currentFile) {
-                    messageChunksList.push(`[Writing code for ${currentFile}...]\n`);
-                } else if (partialMode && currentFile === null) {
-                    messageChunksList.push("[Writing code for");
-                } else {
-                    throw new Error(`Unexpected file name ${currentFile}`);
-                }
-                messageChunksList.push("");
-                searchReplaceList.push(searchReplaces.create(currentFile!,
-                    currentSearch.join("\n") + "\n", // match whole lines only
-                    currentReplace.join("\n") + "\n"
-                ));
-                currentSearch = [];
-                currentReplace = [];
-                currentSection = nextSection(currentSection, "codeChange");
-                break;
-            case "ccClose":
-                currentFile = undefined; // we could maybe relax this later
-                currentSection = nextSection(currentSection, "topLevel");
-                break;
-            case "other":
-                if (currentSection === "search") {
-                    currentSearch.push(line);
-                } else if (currentSection === "replace") {
-                    currentReplace.push(line);
-                } else if (currentSection === "topLevel") {
-                    messageChunksList[messageChunksList.length - 1] += line + "\n";
-                } else {
-                    // otherwise, it's in CodeChange but not in search/replace
-                    console.log("ignoring stray line in CodeChange: ", line);
-                }
-                break;
+        // ENABLED -- raw message strategy (each CC is a chunk)
+        messageChunksList.push("");
+
+        try {
+          currentFile = extractFileName(line);
+        } catch (e) {
+          if (partialMode) {
+            currentFile = null;
+          } else {
+            throw e;
+          }
         }
-    }
+        currentSection = nextSection(currentSection, "codeChange");
+        break;
+      case "srOpen":
+        // // DISABLED - parse message strategy
+        // if (currentFile) {
+        //   messageChunksList.push(`[Writing code for ${currentFile}...]\n`);
+        // } else if (partialMode && currentFile === null) {
+        //   messageChunksList.push("[Writing code for");
+        // } else {
+        //   throw new Error(`Unexpected file name ${currentFile}`);
+        // }
+        // messageChunksList.push("");
+        currentSection = nextSection(currentSection, "search");
+        break;
+      case "srDivider":
+        currentSection = nextSection(currentSection, "replace");
+        break;
+      case "srClose":
+        // // DISABLED -- parsed message strategy
+        // messageChunksList.push("");
 
-    return {
-        messageChunksList,
-        searchReplaceList
-    };
+        searchReplaceList.push(
+          searchReplaces.create(
+            currentFile!,
+            currentSearch.join("\n") + "\n", // match whole lines only
+            currentReplace.join("\n") + "\n"
+          )
+        );
+        currentSearch = [];
+        currentReplace = [];
+        currentSection = nextSection(currentSection, "codeChange");
+        break;
+      case "ccClose":
+        currentFile = undefined; // we could maybe relax this later
+        currentSection = nextSection(currentSection, "topLevel");
+        break;
+      case "other":
+        if (currentSection === "search") {
+          currentSearch.push(line);
+        } else if (currentSection === "replace") {
+          currentReplace.push(line);
+        } else if (currentSection === "topLevel") {
+          // // DISABLED -- parsed message strategy
+          //   messageChunksList[messageChunksList.length - 1] += line + "\n";
+        } else {
+          // otherwise, it's in CodeChange but not in search/replace
+          console.log("ignoring stray line in CodeChange: ", line);
+        }
+        break;
+    }
+    // ENABLED -- raw message strategy (all lines included in chunks)
+    messageChunksList[messageChunksList.length - 1] += line + "\n";
+  }
+
+  return {
+    messageChunksList,
+    searchReplaceList,
+  };
 }
 
 function extractFileName(line: string): string | undefined {
-    const match = line.match(/file="([^"]*)"/);
-    if (!match || match.length !== 2) {
-        throw new Error(`Unable to get filename from: ${line}`); // TODO: relax this to undefined
-    }
-    return stripFilename(match[1]);
+  const match = line.match(/file="([^"]*)"/);
+  if (!match || match.length !== 2) {
+    throw new Error(`Unable to get filename from: ${line}`); // TODO: relax this to undefined
+  }
+  return stripFilename(match[1]);
 }
-
 
 // export function findSearchReplaceBlocks(content: string): SearchReplace[] {
 //     const separators = DIFF_PIECES.join("|");
@@ -355,22 +387,43 @@ function extractFileName(line: string): string | undefined {
 //     }
 // }
 
-function applySearchReplace(gitRepo: GitRepo, pseudoCommit: PseudoCommit, searchReplace: SearchReplace): PseudoCommit {
-    const originalContents = (
-        pseudoCommits.hasFile(gitRepo, pseudoCommit, searchReplace.filePath) ?
-            pseudoCommits.getFileContents(gitRepo, pseudoCommit, searchReplace.filePath) :
-            "\n\n" // so that it has something to search for
-    );
+function applySearchReplace(
+  gitRepo: GitRepo,
+  pseudoCommit: PseudoCommit,
+  searchReplace: SearchReplace
+): PseudoCommit {
+  const originalContents = pseudoCommits.hasFile(
+    gitRepo,
+    pseudoCommit,
+    searchReplace.filePath
+  )
+    ? pseudoCommits.getFileContents(
+        gitRepo,
+        pseudoCommit,
+        searchReplace.filePath
+      )
+    : "\n\n"; // so that it has something to search for
 
-    if (!originalContents.includes(searchReplace.search)) {
-        console.log("search string:");
-        console.log(searchReplace.search);
-        console.log("search string trimmed:");
-        console.log(searchReplace.search.trim());
-        console.log("replace string:");
-        console.log(searchReplace.replace);
-        throw new Error(`Search text not found`);
-    }
-    const updatedContent = originalContents.replace(searchReplace.search, searchReplace.replace);
-    return pseudoCommits.upsertFileContents(pseudoCommit, searchReplace.filePath, updatedContent);
+  if (!originalContents.includes(searchReplace.search)) {
+    console.error("failed to apply diff");
+    console.error("search string:");
+    console.error(searchReplace.search);
+    console.error("search string trimmed:");
+    console.error(searchReplace.search.trim());
+    console.error("replace string:");
+    console.error(searchReplace.replace);
+    vscode.window.showErrorMessage(
+      `Failed to apply diff: search text not found`
+    );
+    return pseudoCommit;
+  }
+  const updatedContent = originalContents.replace(
+    searchReplace.search,
+    searchReplace.replace
+  );
+  return pseudoCommits.upsertFileContents(
+    pseudoCommit,
+    searchReplace.filePath,
+    updatedContent
+  );
 }
