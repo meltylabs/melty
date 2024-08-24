e55b36866614891cf0a5b0dc5a2fd2fa98cf38db
Charlie Holtz <charlieholtz@gmail.com>
2024-08-24 12:52:43 -0700

    Show human diffs above (#129)
    * [via melty] style: Add rounded corners to button component
    
    * [by melty] refactor: Restructure JouleComponent for improved layout and readability
    
    * [via melty] style: Improve formatting and readability in JouleComponent.tsx
    
    * [by melty] feat: Make human diffs collapsible in JouleComponent
    
    * [by melty] refactor: Make only human diffs collapsible in JouleComponent
    
    * add some padding
    
    * [by melty] refactor: Improve layout and styling of JouleComponent
    
    * human wrote some code...

--- webview-ui/src/components/JouleComponent.tsx
+++ webview-ui/src/components/JouleComponent.tsx
@@ -7,6 +7,12 @@ import { Joule } from "../types";
 import CopyButton from "./CopyButton";
 import DiffViewer from "./DiffViewer";
 import { Button } from "./ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+import { CodeXmlIcon } from "lucide-react";
 
 export function JouleComponent({
   joule,
@@ -46,90 +52,111 @@ export function JouleComponent({
     );
   }
 
-  return (
-    <div
-      className={`flex mb-2 p-2 rounded-md ${
-        joule.author === "human"
-          ? "bg-gray-50 border border-gray-200"
-          : "bg-white"
-      }`}
-    >
-      <div
-        className={`${
-          diffHtml ? "w-[40%]" : "w-full"
-        } pr-4 overflow-auto h-full`}
-      >
-        <div className="text-xs prose">
-          <ReactMarkdown
-            components={{
-              code({ node, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || "");
-                if (match && match[1] === "codechange") {
-                  return (
-                    <details>
-                      <summary>Writing code...</summary>
-                      <pre
-                        {...(props as React.DetailedHTMLProps<
-                          React.HTMLAttributes<HTMLPreElement>,
-                          HTMLPreElement
-                        >)}
-                      >
-                        <code className={className}>{children}</code>
-                      </pre>
-                    </details>
-                  );
-                }
-                return match ? (
-                  <div className="relative p-0 max-h-[300px] overflow-y-auto no-scrollbar">
-                    {!isPartial && (
-                      <CopyButton code={String(children).replace(/\n$/, "")} />
-                    )}
-                    <SyntaxHighlighter
-                      language={match[1]}
-                      style={vscDarkPlus}
-                      PreTag="div"
-                      children={String(children).replace(/\n$/, "")}
-                    />
-                  </div>
-                ) : (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                );
-              },
-            }}
-          >
-            {joule.message}
-          </ReactMarkdown>
-        </div>
-      </div>
-      {showDiff && (
-        <div
-          className={`${diffHtml ? "w-[60%]" : "hidden"} overflow-auto h-full`}
-        >
-          {diffHtml && !isPartial && (
-            <>
-              <DiffViewer diff={diffHtml} />
-              <div className="flex justify-between items-center mt-1">
-                <span className="font-mono text-muted-foreground text-xs">
-                  {joule.commit?.substring(0, 7)}
-                </span>
-                {joule.author === "bot" &&
-                  !isPartial &&
-                  isLatestCommit &&
-                  !undoClicked && (
-                    <Button
-                      variant="outline"
-                      size="sm"
-                      onClick={() => {
-                        handleUndo();
-                      }}
-                    >
-                      Undo commit
-                    </Button>
-                  )}
+  const MessageContent = () => (
+    <div className="text-xs prose">
+      <ReactMarkdown
+        components={{
+          code({ node, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            if (match && match[1] === "codechange") {
+              return (
+                <details>
+                  <summary>Writing code...</summary>
+                  <pre
+                    {...(props as React.DetailedHTMLProps<
+                      React.HTMLAttributes<HTMLPreElement>,
+                      HTMLPreElement
+                    >)}
+                  >
+                    <code className={className}>{children}</code>
+                  </pre>
+                </details>
+              );
+            }
+            return match ? (
+              <div className="relative p-0 max-h-[300px] overflow-y-auto no-scrollbar">
+                {!isPartial && (
+                  <CopyButton code={String(children).replace(/\n$/, "")} />
+                )}
+                <SyntaxHighlighter
+                  language={match[1]}
+                  style={vscDarkPlus}
+                  PreTag="div"
+                  children={String(children).replace(/\n$/, "")}
+                />
               </div>
-            </>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {joule.message}
+      </ReactMarkdown>
+    </div>
+  );
+
+  const DiffContent = ({ isHuman }: { isHuman: boolean }) =>
+    isHuman ? (
+      <Collapsible className="bg-white border border-gray-200 rounded-md">
+        <CollapsibleTrigger className="flex items-center text-xs justify-between w-full p-2 bg-white hover:bg-gray-100 rounded-t-md">
+          <span className="flex items-center italic">
+            <CodeXmlIcon className="h-4 w-4 mr-2" />
+            Human wrote some code...
+          </span>
+          <span className="font-mono text-muted-foreground text-xs">
+            {joule.commit?.substring(0, 7)}
+          </span>
+        </CollapsibleTrigger>
+        <CollapsibleContent className="p-2">
+          <DiffViewer diff={diffHtml!} />
+        </CollapsibleContent>
+      </Collapsible>
+    ) : (
+      <>
+        <DiffViewer diff={diffHtml!} />
+        {!isPartial && isLatestCommit && !undoClicked && (
+          <div className="mt-2">
+            <Button variant="outline" size="sm" onClick={handleUndo}>
+              Undo commit
+            </Button>
+          </div>
+        )}
+      </>
+    );
+
+  return (
+    <div className="mb-2">
+      {joule.author === "human" && diffHtml ? (
+        <div className="flex flex-col">
+          <div className="w-full mb-2">
+            <DiffContent isHuman={true} />
+          </div>
+          <div className="w-full p-2 bg-gray-50 border border-gray-200 rounded-md">
+            <MessageContent />
+          </div>
+        </div>
+      ) : (
+        <div
+          className={`flex p-2 rounded-md ${
+            joule.author === "human"
+              ? "bg-gray-50 border border-gray-200"
+              : "bg-white"
+          }`}
+        >
+          <div
+            className={`${
+              diffHtml ? "w-[40%]" : "w-full"
+            } pr-4 overflow-auto h-full`}
+          >
+            <MessageContent />
+          </div>
+          {showDiff && diffHtml && (
+            <div className="w-[60%] overflow-auto h-full">
+              <DiffContent isHuman={false} />
+            </div>
           )}
         </div>
       )}
