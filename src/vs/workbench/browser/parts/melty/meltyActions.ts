81fa80650dc3a525cde7a7e0d8dba122bbb97d5b
Jackson <jacksondc@users.noreply.github.com>
2024-08-27 10:43:02 -0700

    add cmd+m keybinding (#9)

--- src/vs/workbench/browser/parts/melty/meltyActions.ts
+++ src/vs/workbench/browser/parts/melty/meltyActions.ts
@@ -1,6 +1,7 @@
 import { registerAction2, Action2 } from 'vs/platform/actions/common/actions';
 import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
 import { IMeltyService } from './meltyService';
+import { KeyCode, KeyMod } from 'vs/base/common/keyCodes';
 
 export class ToggleMeltyAction extends Action2 {
 	static readonly ID = 'workbench.action.toggleMelty';
@@ -9,7 +10,11 @@ export class ToggleMeltyAction extends Action2 {
 		super({
 			id: ToggleMeltyAction.ID,
 			title: { value: 'Toggle Melty Popup', original: 'Toggle Melty Popup' },
-			f1: true
+			f1: true,
+			keybinding: {
+				weight: 250,
+				primary: KeyMod.CtrlCmd | KeyCode.KeyM,
+			}
 		});
 	}
 
