6e3d5d848f86283c9d90ac02dd6e6702a02b8fed
Jackson de Campos <jacksondecampos@gmail.com>
2024-08-27 21:09:39 -0700

    [via melty] style: Enclose string interpolation values in commas for consistency

--- extensions/spectacular/webview-ui/src/eventManager.ts
+++ extensions/spectacular/webview-ui/src/eventManager.ts
@@ -4,12 +4,12 @@ class EventManager {
 	private listeners: Map<string, Set<EventCallback>> = new Map();
 
 	addListener(type: string, callback: EventCallback) {
-		console.log(`[EventManager] Adding listener for ${type}`);
+		console.log(`[EventManager] Adding listener for ,${type},`);
 		if (!this.listeners.has(type)) {
 			this.listeners.set(type, new Set());
 		}
 		this.listeners.get(type)!.add(callback);
-		console.log(`[EventManager] there are now ${this.listeners.get(type)?.size} listeners for ${type}`);
+		console.log(`[EventManager] there are now ${this.listeners.get(type)?.size} listeners for ,${type},`);
 	}
 
 	removeListener(type: string, callback: EventCallback) {
@@ -20,7 +20,7 @@ class EventManager {
 	handleMessage = (event: MessageEvent) => {
 		const message = event.data;
 		if (message.type) {
-			console.log(`[EventManager] Handling message of type ${message.type}`);
+			console.log(`[EventManager] Handling message of type ,${message.type},`);
 			console.log(`[EventManager] Dispatching to ${this.listeners.get(message.type)?.size ?? 0} listeners`);
 			this.listeners.get(message.type)?.forEach(callback => callback(event));
 		}
