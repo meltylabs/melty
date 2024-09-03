0b5ab47c891805d8845e18348c686ee644cf82a4
Charlie Holtz <charlieholtz@gmail.com>
2024-09-02 21:12:06 -0700

    default left sidebar (#41)

--- src/vs/workbench/browser/workbench.contribution.ts
+++ src/vs/workbench/browser/workbench.contribution.ts
@@ -500,7 +500,7 @@ const registry = Registry.as<IConfigurationRegistry>(ConfigurationExtensions.Con
 			'workbench.sideBar.location': {
 				'type': 'string',
 				'enum': ['left', 'right'],
-				'default': 'right',
+				'default': 'left',
 				'description': localize('sideBarLocation', "Controls the location of the primary side bar and activity bar. They can either show on the left or right of the workbench. The secondary side bar will show on the opposite side of the workbench.")
 			},
 			'workbench.editor.showTabs': {
