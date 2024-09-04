cd86a0555e2796b62bc0eb08553b4836507e2b4a
Charlie Holtz <charlieholtz@gmail.com>
2024-09-04 14:32:45 -0700

    [by melty] feat: Add build script for Melty project

--- melty_build.sh
+++ melty_build.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# Run yarn
+yarn
+
+# Navigate to spectacular extension and install dependencies
+cd extensions/spectactular && npm i
+
+# Navigate to webview-ui and install dependencies
+cd webview-ui && npm i
+
+# Return to spectacular directory and build
+cd .. && npm run build
+
+# Navigate to supermaven extension and install dependencies
+cd ../supermaven && npm i
+
+# Return to the root directory
+cd ../..
