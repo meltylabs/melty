367d0cf4a73247ff1db4c79b31cdaeb1ff05a53e
Charlie Holtz <charlieholtz@gmail.com>
2024-09-06 11:21:28 -0700

    feat: Add melty_setup.sh script for project initialization

---
+++ scripts/melty_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run yarn
+yarn
+
+# Navigate to spectacular extension and install dependencies
+cd extensions/spectacular && npm i
+
+# Navigate to webview-ui and install dependencies
+cd webview-ui && npm i
+
+# Return to spectacular directory and build
+cd .. && npm run compile && npm run build:webview
+
+# Navigate to supermaven extension and install dependencies
+cd ../supermaven && npm i
+
+# Return to the root directory
+cd ../..
