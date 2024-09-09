07c9308a279d62049188c41953364f0874dcb815
Jackson de Campos <jacksondecampos@gmail.com>
2024-09-09 13:03:51 -0700

    use non-ci build tasks

--- scripts/melty/build.sh
+++ scripts/melty/build.sh
@@ -13,9 +13,10 @@ yarn gulp compile-extension-media
 yarn gulp compile-extensions-build
 yarn gulp minify-vscode
 
-yarn gulp "vscode-darwin-${VSCODE_ARCH}-min-ci"
+yarn gulp "vscode-darwin-${VSCODE_ARCH}-min"
 
+# update modification time
 find "../VSCode-darwin-${VSCODE_ARCH}" -print0 | xargs -0 touch -c
 
 yarn gulp minify-vscode-reh
-yarn gulp "vscode-reh-${VSCODE_PLATFORM}-${VSCODE_ARCH}-min" # removed -ci
+yarn gulp "vscode-reh-${VSCODE_PLATFORM}-${VSCODE_ARCH}-min"
