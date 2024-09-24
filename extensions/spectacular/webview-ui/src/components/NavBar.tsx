83f3d1fe6650f2e813b33a265ddafc6370108f83
Charlie Holtz <charlieholtz@gmail.com>
2024-09-24 14:00:08 -0700

    updated nav

--- extensions/spectacular/webview-ui/src/components/NavBar.tsx
+++ extensions/spectacular/webview-ui/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { HelpCircle } from 'lucide-react';
+import { HelpCircle, HomeIcon } from 'lucide-react';
 
 export const NavBar: React.FC = () => {
 	const location = useLocation();
@@ -8,32 +8,10 @@ export const NavBar: React.FC = () => {
 
 	const isHelpPage = location.pathname === '/help';
 
-	const toggleHelp = () => {
-		if (isHelpPage) {
-			navigate('/');
-		} else {
-			navigate('/help');
-		}
-	};
 
 	return (
-		<nav className="mb-6 mt-4 mx-3 relative">
-			<ul className="flex justify-between">
-				<li></li>
-				<li>
-					<Link to="/">
-						<h1 className="text-3xl font-extrabold tracking-tighter text-center">
-							melty
-						</h1>
-					</Link>
-				</li>
-				<li>
-				</li>
-			</ul>
+		<nav className="mb-2 mt-2 h-7 bg-gray-50 flex items-center justify-end relative border-b border-gray-200">
 
-			<button onClick={toggleHelp} className="absolute right-4 top-2">
-				<HelpCircle />
-			</button>
 		</nav>
 	);
 };
