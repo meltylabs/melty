2c728b688bd9f5df7646c81441ab362eed0070d6
Charlie Holtz <charlieholtz@gmail.com>
2024-09-24 12:41:53 -0700

    fix ring

--- extensions/spectacular/webview-ui/src/components/ConversationView.tsx
+++ extensions/spectacular/webview-ui/src/components/ConversationView.tsx
@@ -450,7 +450,7 @@ export function ConversationView() {
 							handleDropFile={handleDropFile}
 							placeholder="Talk to Melty"
 							id="message"
-							className="p-3 pr-12 pb-12 relative focus-visible:ring-1 max-h-[30vh] overflow-y-auto"
+							className="p-3 pr-12 pb-12 relative focus-visible:ring-0 max-h-[30vh] overflow-y-auto"
 							ref={inputRef}
 							required
 							value={messageText}
